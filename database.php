9dafece32dd8fad27286658b22c6dba37fd53374
NastasjaBakovic1999 <nastasjabakovic199@gmail.com>
2021-12-08 00:39:17 +0100

    dodata fja za rastuce sortiranje u database

--- database.php
+++ database.php
@@ -110,4 +110,16 @@
     }
     return  $data;
 }
+
+	public function prikazSortiranjeOpadajuce(){
+    $data=null;
+	$query="SELECT k.KnigaID, k.Naziv,k.Autor,k.Zanr,i.NazivIK as NazivIK 
+			from knjige k join izdavac i on i.ID=k.IzdavacID order by k.Naziv desc";
+	if($sql=$this->dblink->query($query)){
+    while($row=mysqli_fetch_assoc($sql)){
+    $data[]=$row;
+    }
+    }
+    return  $data;
+}
 ?>
