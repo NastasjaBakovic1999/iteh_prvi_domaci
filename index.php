170ac477c07fba27c3d09f81833372293981c279
NastasjaBakovic1999 <nastasjabakovic199@gmail.com>
2021-12-08 00:10:59 +0100

    dodat nav bar

--- index.php
+++ index.php
@@ -10,7 +10,25 @@
     <title>Moja knjižarica</title>
     <link rel="shortcut icon" type="image/jpg" href="book.png"/>
   </head>
-  <body style="background-image: url('knjizara.jpg'); background-repeat: no-repeat; background-position: center;background-size: cover;">
+  	<body style="background-image: url('knjizara.jpg'); background-repeat: no-repeat; background-position: center;background-size: cover;">
+	<ul class="nav justify-content-end" style="background-color:rgba(0, 0, 0,0.6);">
+ 		<li class="nav-item">
+    		<a class="nav-link active" href="knjizara.php" style="color:white">Knjižara</a>
+  	</li>
+	</ul>
 
+   <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+	
+	<script>
    </body>
 </html>
